7d51010de8bdb7d2fb87ff15120c50f86348c36f
James Cryer <james.cryer@huddle.com>
2015-01-14 17:11:30 +0000

    exposing hideElements property

--- tasks/phantomflow.js
+++ tasks/phantomflow.js
@@ -1,37 +1,36 @@
-'use strict';
-
 /*
  * grunt-phantomflow
  * Copyright (c) 2014 Huddle
  * Licensed under The MIT License (MIT).
  */
 
-module.exports = function(grunt) {
+module.exports = function ( grunt ) {
 
-	grunt.registerMultiTask('phantomflow', 'UI testing with decision trees', function() {
+	grunt.registerMultiTask( 'phantomflow', 'UI testing with decision trees', function () {
 
-		var phantomflow = require('phantomflow').init({
-			test: grunt.option('test'), // to run a specific test
-			debug: grunt.option('debug'),
+		var phantomflow = require( 'phantomflow' ).init( {
+			test: grunt.option( 'test' ), // to run a specific test
+			debug: grunt.option( 'debug' ),
 			createReport: this.data.createReport,
 			includes: this.data.includes,
 			tests: this.data.tests,
 			results: this.data.results,
-			threads: grunt.option('threads') || this.data.threads,
-			earlyExit: grunt.option('earlyexit'),
-			novisuals: grunt.option('novisuals')
-		});
+			threads: grunt.option( 'threads' ) || this.data.threads,
+			earlyExit: grunt.option( 'earlyexit' ),
+			novisuals: grunt.option( 'novisuals' ),
+			hideElements: this.data.hideElements || [],
+		} );
 
 		var done = this.async();
 
-		if(grunt.option('report')){
+		if ( grunt.option( 'report' ) ) {
 			phantomflow.report();
 			return;
 		}
 
-		phantomflow.run(function(code){
-			done(code == 0);
-		});	
+		phantomflow.run( function ( code ) {
+			done( code == 0 );
+		} );
 
-	});
+	} );
 };
